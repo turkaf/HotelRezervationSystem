39630c82cb6566fdf51bb3f48df19ec8b4f905d9
Yunus Emre <mojo_jojo06@hotmail.com>
2024-12-08 18:52:36 +0300

    Abstract, EF, Repository, Concrete Files Added

---
+++ DataAccessLayer/Abstract/IGeneric.cs
@@ -0,0 +1,16 @@
+?using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetList();
+    }
+}
---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,45 @@
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        // This method deletes the given entity from the database.
+        public void Delete(T entity)
+        {
+            using var context = new Context();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        // This method retrieves all entities from the database.
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        // This method inserts the given entity into the database.
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        // This method updates the given entity in the database.
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
